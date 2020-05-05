0894a3fece75d097a0c819fc183c3bb9e5a4c383
Jacob Herper <jacob@herper.uk>
2020-05-05 23:04:59 +0100

    Linting fixed

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -14,12 +14,13 @@ const Timeline = () => {
     );
 
     const fetchData = () => {
-      postsRequest.poll(2000).get()
+      postsRequest
+        .poll(2000)
+        .get()
         .then(({ data }: any) => {
-          const mergedArrays = [
-            ...posts,
-            ...data,
-          ].sort(function (a, b) { return b.id - a.id });
+          const mergedArrays = [...posts, ...data].sort(function (a, b) {
+            return b.id - a.id;
+          });
 
           const uniquePosts = unionBy(mergedArrays, 'id');
 
@@ -29,24 +30,26 @@ const Timeline = () => {
           console.error('Something went wrong!', err);
           fetchData();
         });
-    }
+    };
     fetchData();
-  }, [posts])
+  }, [posts]);
 
   return (
     <div>
-      {posts.length > 0
-        ? posts.map(({ id, image, text, timeStamp, username }: IPost) => (
-            <Card
-              key={id}
-              id={id}
-              image={image}
-              text={text}
-              timeStamp={timeStamp}
-              username={username}
-            />
-          ))
-        : <p>Loading...</p>}
+      {posts.length > 0 ? (
+        posts.map(({ id, image, text, timeStamp, username }: IPost) => (
+          <Card
+            key={id}
+            id={id}
+            image={image}
+            text={text}
+            timeStamp={timeStamp}
+            username={username}
+          />
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 };
