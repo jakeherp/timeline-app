430e63fd4b0b5f17cf4cb503e9bb69135f3ca8c8
Jacob Herper <jacob@herper.uk>
2020-05-05 22:26:46 +0100

    Implemented axios-request-handler

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import Request from 'axios-request-handler';
+import unionBy from 'lodash.unionby';
 import Card from './Card';
 
 import { useFetch, useInterval } from '../utils';
@@ -8,18 +10,30 @@ import { IPost } from '../interfaces/post';
 const Timeline = () => {
   const [posts, setPosts] = useState<IPost[]>([]);
 
-  const { response, loading, error } = useFetch(
-    'https://magiclab-twitter-interview.herokuapp.com/jacob-herper/api?count=100'
+  const postsRequest = new Request(
+    'https://magiclab-twitter-interview.herokuapp.com/jacob-herper/api'
   );
 
   useEffect(() => {
-    if (response === null) return;
-    setPosts(response);
-  }, [response]);
+    postsRequest.poll(5000).get()
+      .then(({ data }: any) => {
+        const mergedArrays = [
+          ...posts,
+          ...data,
+        ]
+
+        const uniquePosts = unionBy(mergedArrays, 'id').sort(function (a, b) { return b.id - a.id });
+
+        setPosts(uniquePosts);
+      }, () => {
+        console.error('Something went wrong!');
+      });
+  }, [posts])
+
+  console.log(posts);
 
   return (
     <div>
-      {error && <p>Something went wrong...</p>}
       {posts.length > 0
         ? posts.map(({ id, image, text, timeStamp, username }: IPost) => (
             <Card
@@ -31,7 +45,7 @@ const Timeline = () => {
               username={username}
             />
           ))
-        : loading && <p>Loading...</p>}
+        : <p>Loading...</p>}
     </div>
   );
 };
